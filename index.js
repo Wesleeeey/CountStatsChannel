3a37d8d76d1bdf0e77f866d7aebfc08091fda448
Wesley <71722269+Wesleeeey@users.noreply.github.com>
2021-12-28 10:03:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,26 +10,12 @@ client.on('ready', async () => {
     var server = client.guilds.cache.get(guild);
     setInterval(() => {
     const memberCount = server.memberCount
-    client.channels.cache.get(stats_member).setName(`🌹・Membres : ${memberCount.toLocaleString()}`)
-    console.log(`Update Stats Member`)
-    }, 300000);
-})
-
-client.on('ready', async () => {
-    var server = client.guilds.cache.get(guild);
-    setInterval(() => {
-        const onlineCount = server.members.cache.filter(member => member.presence.status !== "offline").size
-    client.channels.cache.get(stats_online).setName(`🌟・Online : ${onlineCount.toLocaleString()}`)
-    console.log(`Update Stats Online`)
-    }, 300000);
-})
-
-client.on('ready', async () => {
-    var server = client.guilds.cache.get(guild);
-    setInterval(() => {
+    const onlineCount = server.members.cache.filter(member => member.presence.status !== "offline").size
     const voiceCount = server.members.cache.filter(m => m.voice.channel).size
+    client.channels.cache.get(stats_member).setName(`🌹・Membres : ${memberCount.toLocaleString()}`)
+    client.channels.cache.get(stats_online).setName(`🌟・Online : ${onlineCount.toLocaleString()}`)
     client.channels.cache.get(stats_voice).setName(`🐺・Vocal : ${voiceCount.toLocaleString()}`)
-    console.log(`Update Stats Voice`)
+    console.log(`Update Stats Member`)
     }, 300000);
 })
 
